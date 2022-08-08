aaed37b7c39bd6baf5b4fb7ff916aa01435af6d4
Muhammad Sarwar <rrsarwar109@gmail.com>
2022-08-09 02:25:40 +0500

    f

--- src/services/peer.ts
+++ src/services/peer.ts
@@ -1,7 +1,4 @@
 import { Peer } from 'peerjs';
-const peerClient = new Peer({
-	host: 'localhost',
-	port: 8001,
-	path: '/peerjs',
-});
-export default peerClient;
+export default function initPeer() {
+	return;
+}
