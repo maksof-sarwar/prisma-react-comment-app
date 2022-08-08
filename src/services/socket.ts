7a601944c38ae72efe7ee288e97e720a6bcb219a
Muhammad Sarwar <rrsarwar109@gmail.com>
2022-08-09 02:57:26 +0500

    a

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,5 +1,3 @@
 import io from 'socket.io-client';
-const socket = io('https://hsi-chat-server.herokuapp.com', {
-	transports: ['polling', 'websocket'],
-});
+const socket = io('https://hsi-chat-server.herokuapp.com');
 export default socket;
